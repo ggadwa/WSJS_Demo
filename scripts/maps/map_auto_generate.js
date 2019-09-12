36de4660bf3b595ce58fecd462ed21483573ad33
Brian Barnes <ggadwa@charter.net>
2019-09-12 18:14:14 -0400

    09_12_2019

--- scripts/maps/map_auto_generate.js
+++ scripts/maps/map_auto_generate.js
@@ -9,11 +9,17 @@ export default class MapAutoGenerateClass extends ProjectMapClass
         return(
             {
                 "name":"auto_generate_test",
-                "scale":700,
                 "skyBox":{"size":50000,"bitmap":"textures/skybox_test.png"},
                 "autoGenerate":
                     {
-                        "temp":"temp"
+                        "randomSeed":15,
+                        "roomCount":15,
+                        "roomSize":60000,
+                        "roomHeight":20000,
+                        "stairFactor":0.5,
+                        "wallBitmap":{"color":"textures/medieval_brick_color.png","normals":"textures/medieval_brick_normals.png","specular":"textures/medieval_brick_specular.png","specularFactor":{"red":2,"green":2,"blue":2}},
+                        "floorBitmap":{"color":"textures/terracotta_color.png","normals":"textures/terracotta_normals.png","specular":"textures/terracotta_specular.png","specularFactor":{"red":4,"green":4,"blue":4}},
+                        "ceilingBitmap":{"color":"textures/corrugated_metal_color.png","normals":"textures/corrugated_metal_normals.png","specular":"textures/corrugated_metal_specular.png","specularFactor":{"red":5,"green":5,"blue":5}}
                     },
                 "entities":
                     [
